49bfcc1409946f2cb24e346ac043b15bd4c71045
Adu03 <dujiahui2010@aliyun.com>
2025-09-22 00:53:12 +0800

    (update) vite.comfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JsonRun/', // GitHub Pages仓库名称
+  base: '/JsonRun/', // GitHub Pages仓库名称 - Adu03/JsonRun
   plugins: [react()],
   build: {
     rollupOptions: {
